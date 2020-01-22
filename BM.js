20e391d782a573219fb5e48de9f615918dd7dac3
Trevor Lane <tlane@simplenexus.com>
2020-01-21 19:00:57 -0700

    latest

--- BM.js
+++ BM.js
@@ -9,6 +9,7 @@ class BM extends Component {
   constructor(props) {
     super(props);
     this.manager = new BleManager();
+    this.device = null;
     this.state = {info: '', values: {}, message: ''};
     this.prefixUUID = 'f000aa';
     this.suffixUUID = '-0451-4000-b000-000000000000';
@@ -20,7 +21,11 @@ class BM extends Component {
       4: 'Barometer',
       5: 'Gyroscope',
     };
+    this.volChar = '088a1e2a-1991-411e-8a73-f71bdcd7487c';
+    this.soundMessage = 'no sound detected';
   }
+
+
 
   serviceUUID(num) {
     return this.prefixUUID + num + '0' + this.suffixUUID;
@@ -34,52 +39,118 @@ class BM extends Component {
     return this.prefixUUID + num + '2' + this.suffixUUID;
   }
 
-  componentDidMount() {
-   // Alert.alert('BLE Manager mounted');
-  }
-
   info(message) {
     this.setState({info: message});
   }
 
   error(message) {
-    this.setState({info: 'ERROR: ' + message});
+    this.setState({info: '', message: ''});
+  }
+
+  disconnect() {
+    if (this.device) {
+      this.device.cancelConnection();
+      this.setState({info: '', message: ''});
+      this.device = null;
+    } else {
+      console.log('no device detected');
+    }
   }
 
   updateValue(key, value) {
     const decodedVal = base64.decode(value);
     console.log('value', decodedVal);
-   
-      this.setState({
-        message: decodedVal,
+    this.setState({
+      message: decodedVal,
     });
     // this.setState({values: {...this.state.values, [key]: value}});
   }
 
-  async setupNotifications(device) {
-    for (const id in this.sensors) {
-      const service = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; //this.serviceUUID(id);
-      const characteristicW = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; // this.writeUUID(id);
-      const characteristicN = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; // this.notifyUUID(id);
+  async on() {
+    const rCHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+    const service = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; //this.serviceUUID(id);
 
-      const characteristic = await device.writeCharacteristicWithResponseForService(
+    if (this.device) {
+      await this.device.writeCharacteristicWithResponseForService(
         service,
-        characteristicW,
-        'AQ==' /* 0x01 in hex */,
+        this.volChar,
+        base64.encode('1'),
       );
-
-      device.monitorCharacteristicForService(
-        service,
-        characteristicN,
-        (error, characteristic) => {
-          if (error) {
-            this.error(error.message);
-            return;
-          }
-          this.updateValue(characteristic.uuid, characteristic.value);
-        },
-      );
+    } else {
+      console.log('device not detected');
     }
+  }
+
+  async off() {
+    const rCHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+    const service = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; //this.serviceUUID(id);
+
+    if (this.device) {
+      await this.device.writeCharacteristicWithResponseForService(
+        service,
+        this.volChar,
+        base64.encode('0'),
+      );
+    } else {
+      console.log('device not detected');
+    }
+  }
+
+  async fetchValue() {
+    console.log('fetching val');
+    const service = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; //this.serviceUUID(id);
+    const characteristicW = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; // this.writeUUID(id);
+    const characteristicN = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; // this.notifyUUID(id);
+
+    if (this.device) {
+      this.device
+        .readCharacteristicForService(service, characteristicN)
+        .then(res => {
+          console.log('val', base64.decode(res.value));
+          this.setState({
+            message: base64.decode(res.value),
+          });
+        });
+
+      //   (error, characteristic) => {
+      //     if (error) {
+      //       this.error(error.message);
+      //       return;
+      //     }
+      //     this.updateValue(characteristic.uuid, characteristic.value);
+      //   },
+      // );
+    } else {
+      console.log('no device detected');
+    }
+  }
+
+  componentDidMount() {
+    this.soundMessage = setInterval(() => this.fetchValue(), 500);
+  }
+
+  async setupNotifications(device) {
+    const service = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; //this.serviceUUID(id);
+    const characteristicW = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; // this.writeUUID(id);
+    const characteristicN = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; // this.notifyUUID(id);
+
+    const characteristic = await device.writeCharacteristicWithResponseForService(
+      service,
+      characteristicW,
+      'AQ==',
+    );
+
+    device.monitorCharacteristicForService(
+      service,
+      characteristicN,
+      (error, characteristic) => {
+        if (error) {
+          this.error(error.message);
+          return;
+        }
+        this.updateValue(characteristic.uuid, characteristic.value);
+      },
+    );
   }
 
   scanAndConnect() {
@@ -105,6 +176,7 @@ class BM extends Component {
           })
           .then(device => {
             this.info('Setting notifications');
+            this.device = device;
             return this.setupNotifications(device);
           })
           .then(() => {
@@ -139,7 +211,7 @@ class BM extends Component {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Button
-        style={{top: 50}}
+          style={{top: 50}}
           title="Scan For Devices"
           onPress={() => this.scanAndConnect()}
         />
@@ -154,6 +226,25 @@ class BM extends Component {
             </Text>
           );
         })} */}
+        <Button style={{top: 50}} title="On" onPress={() => this.on()} />
+        <Text>{'\n\n'}</Text>
+        <Button style={{top: 50}} title="Off" onPress={() => this.off()} />
+        <Text>{'\n\n'}</Text>
+        <Button
+          style={{top: 50}}
+          title="fetch val"
+          onPress={() => this.fetchValue()}
+        />
+        <Text>
+          {this.device
+            ? '\n\n' + this.device.name + '\n\n'
+            : '\n\n no device detected\n\n'}
+        </Text>
+        <Button
+          style={{top: 50}}
+          title="Disconnect"
+          onPress={() => this.disconnect()}
+        />
       </View>
     );
   }
